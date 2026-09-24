3f47b79a58fdee00061977cf4647c94388908364
dilovar6666 <dilovar6666@users.noreply.github.com>
2026-09-24 22:41:31 +0400

    feat: add custom user models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,25 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    def __str__(self):
+        return self.username
+
+
+class UserCar(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="saved_cars",
+    )
+    car = models.ForeignKey(
+        "cars.Car",
+        on_delete=models.CASCADE,
+        related_name="saved_by_users",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.car}"
