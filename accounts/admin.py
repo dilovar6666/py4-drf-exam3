c5f33f80bf8819c4900c1a6cd0c5c4fc809c6bf6
dilovar6666 <dilovar6666@users.noreply.github.com>
2026-09-25 14:24:58 +0400

    admin: configure accounts admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser, UserCar
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    pass
+
+
+@admin.register(UserCar)
+class UserCarAdmin(admin.ModelAdmin):
+    list_display = ("user", "car", "created_at")
+    list_filter = ("created_at",)
+    search_fields = (
+        "user__username",
+        "user__email",
+        "car__car_model__name",
+        "car__car_model__brand__name",
+    )
+    list_select_related = ("user", "car__car_model__brand")
